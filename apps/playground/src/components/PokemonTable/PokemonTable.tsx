b20d4ec1e38c20d800ea08902110eed5503fd1bf
Simen Abelsen <siab@equinor.com>
2022-11-21 22:12:29 +0100

    ⚗️ Add example using jotai providers

--- apps/playground/src/components/PokemonTable/PokemonTable.tsx
+++ apps/playground/src/components/PokemonTable/PokemonTable.tsx
@@ -1,18 +1,49 @@
-import { DataTable } from '@equinor/apollo-components'
+import {
+  DataTable,
+  globalFilterAtom,
+  rowSelectionAtom,
+  tableSortingAtom,
+} from '@equinor/apollo-components'
 import { EdsProvider } from '@equinor/eds-core-react'
+import { Provider, useAtom } from 'jotai'
 import { pokemon } from '../../data'
 import { pokemonColumns } from './columns'
 
 export const PokemonTable = () => {
   return (
     <EdsProvider density="compact">
-      <DataTable
-        columns={pokemonColumns}
-        config={{ sortable: true, virtual: true, height: '800px', enableRowSelection: true }}
-        data={pokemon}
-        filters={{ globalFilter: true }}
-        header={{ stickyHeader: true, tableCaption: 'Pokédex' }}
-      />
+      <div>
+        <Provider>
+          <DataTable
+            columns={pokemonColumns}
+            config={{ sortable: true, virtual: true, height: '400px', enableRowSelection: true }}
+            data={pokemon}
+            filters={{ globalFilter: true }}
+            header={{ stickyHeader: true, tableCaption: 'Pokédex' }}
+          />
+          <DebugTableAtoms name="Pokédex" />
+        </Provider>
+        <Provider>
+          <DataTable
+            columns={pokemonColumns}
+            config={{ sortable: true, virtual: true, height: '400px', enableRowSelection: true }}
+            data={pokemon}
+            filters={{ globalFilter: true }}
+            header={{ stickyHeader: true, tableCaption: 'Pokédex 2 Electric Boogaloo' }}
+          />
+          <DebugTableAtoms name="Pokédex 2" />
+        </Provider>
+      </div>
     </EdsProvider>
   )
+}
+
+function DebugTableAtoms({ name }: { name?: string }) {
+  const [selectedRows] = useAtom(rowSelectionAtom)
+  const [tableSorting] = useAtom(tableSortingAtom)
+  const [globalFilter] = useAtom(globalFilterAtom)
+
+  console.log({ selectedRows, tableSorting, globalFilter })
+
+  return null
 }
