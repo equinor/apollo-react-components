ebf5d9fb8c34211c1d1e847dcf3062928f01b943
Simen Abelsen <siab@equinor.com>
2023-02-20 13:38:46 +0100

    ✨ Add validateAsync to validator util

--- packages/apollo-utils/src/zod-validation/utils.ts
+++ packages/apollo-utils/src/zod-validation/utils.ts
@@ -6,12 +6,24 @@ export function createValidator<S extends z.ZodTypeAny>(schema: S) {
     validate: <E extends z.infer<typeof schema>>(entity: E) => {
       const validation = schema.safeParse(entity)
       if (validation.success) return undefined
-      return new Map(
-        validation.error.errors.map((error) => [
-          error.path[0] as keyof E,
-          { message: error.message, code: error.code },
-        ])
-      ) as ValidationErrorMap<E>
+      return prepareErrors<E>(validation)
+    },
+    validateAsync: async <E extends z.infer<typeof schema>>(entity: z.infer<typeof schema>) => {
+      const validation = await schema.safeParseAsync(entity)
+      if (validation.success) return undefined
+      return prepareErrors<E>(validation)
+    },
+    getSchema() {
+      return schema
     },
   }
+}
+
+function prepareErrors<E extends Record<string, unknown>>(errorValidation: z.SafeParseError<E>) {
+  return new Map(
+    errorValidation.error.errors.map((error) => [
+      error.path[0] as keyof E,
+      { message: error.message, code: error.code },
+    ])
+  ) as ValidationErrorMap<E>
 }
